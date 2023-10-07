081f868640859770ba772b340012bdbf8724fad6
Rahul Raj <rahulr7177@gmail.com>
2023-10-07 11:51:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ var crsr = document.querySelector(".cursor")
 var main = document.querySelector(".main")
 document.addEventListener("mousemove",function(dets){
     crsr.style.left = (dets.x) +"px";
-    crsr.style.top = (dets.y - 5) +"px";
+    crsr.style.top = (dets.y) +"px";
 });
 
 
